724c8aa5ce0b3ee81314c450224d6726762ad5fd
DEV <DEV@NSN1191261183.edu119.local>
2026-05-14 16:51:20 -0300

    avaliação trimestral

---
+++ A_Anatomia_das_Funções_em_Csharp.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+Console.Write("óla, vamos caluclar quanto dará a soma das duas armas\n");
+
+Console.Write("Digite o valor do primeiro equipamento:");
+int arma1 = lerMoedas();
+if (arma1 == 0)
+{
+    Console.WriteLine($"tentativa de goblin");
+    Thread.Sleep(500);
+    return;
+}
+Console.Write("Digite o valor do segundo equipamento:");
+int arma2 = lerMoedas(); 
+
+
+int resultado = SomarEquipamentos(arma1, arma2);
+
+Console.WriteLine($"O preço final dos equipamentos é: {resultado} moedas!");
+
+
+int lerMoedas()
+
+{
+    if (int.TryParse(Console.ReadLine(), out int n))
+    {
+        return n;
+    }
+   else { return 0; }
+}
+
+int SomarEquipamentos ( int arma1, int arma2)
+{
+return arma1 + arma2;
+}
+
---
+++ Grimôrio.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[][] matrizA = new int[2][];
+matrizA[0][0] = 1;
+matrizA[0][1] = 2;
+matrizA[1][0] = 3;
+matrizA[1][1] = 4;
+
+for(int i = 0; i < matrizA.Length; i++)
+{
+    for(int j = 0; i< matrizA.Length; j++)
+    {
+
+    }
+}
